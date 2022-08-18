97634be289d1d87b77af4f9199a6f909d04c7fd6
Yian <1642904217@qq.com>
2022-08-18 15:29:31 +0800

    My first commit 2.c

---
+++ 2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "string.h"
+#include "stdint.h"
+
+int main(){
+    uint8_t a,b;
+    uint16_t c;
+    a = 0x55;
+    b = 0xaa;
+    c = (a<<8)+b;
+    printf("%x\n\r",c);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
